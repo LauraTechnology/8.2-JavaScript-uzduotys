8591df12966eb80b363a933148be603578195477
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-11 07:42:30 +0300

    Išspręsta 03-2 užduotis

---
+++ mini-uzduotys.js/03-2-ir-3-uzduotys.js
@@ -0,0 +1,14 @@
+// 2.	Išvesti teksto tipo kintamųjų ilgius
+const vaisiai = ['obuolys', 'uoga', 'citrina', 'abrikosas'];
+console.log(vaisiai);
+
+const vaisiaiIlgis = vaisiai.length;
+console.log(vaisiaiIlgis);
+
+// 3.	Tarpusavyje palyginti teksto tipo kintamųjų ilgius:
+// a.	kuris didesnis
+// b.	kuris mažesnis
+// c.	ar jie lygūs
+// d.	ar jie nelygūs
+// e.	kuris didesnis arba lygus
+// f.	kuris mažesnis arba lygus
