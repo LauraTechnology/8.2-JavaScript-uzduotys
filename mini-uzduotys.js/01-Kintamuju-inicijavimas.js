8c43f2c8836dc3002f794c1dd0db8bc10d9a85eb
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-14 11:11:54 +0300

    Išspręsta

---
+++ mini-uzduotys.js/01-Kintamuju-inicijavimas.js
@@ -0,0 +1,52 @@
+// 1.Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+// Po kiekvieno jų inicijavimo, išvesti į console
+const pirmadienis = 1;
+console.log(pirmadienis);
+
+const antradienis = 2;
+console.log(antradienis);
+
+const treciadienis = 3;
+console.log(treciadienis);
+
+console.log('-------------------');
+
+//Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const a = 'zalia';
+console.log(a);
+
+const b = 'aukstai';
+console.log(b);
+
+const c = 'grazu';
+console.log(c);
+
+console.log('-------------------');
+
+//Sukurti 3 sąrašo tipo (array) kintamuosius su penkiomis skaičių tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const menesioDienos = [15, 23, 6, 12, 11];
+console.log(menesioDienos);
+
+const savaitesDienos = [1, 2, 3, 4, 5];
+console.log(savaitesDienos);
+
+const darboDienos = [4, 8, 10, 12, 14];
+console.log(darboDienos);
+
+console.log('-------------------');
+
+//Sukurti 3 sąrašo tipo (array) kintamuosius su penkiomis teksto tipo reikšmėmis
+//Po kiekvieno jų inicijavimo, išvesti į console
+
+const savaite = ['pirmadienis', 'antradienis', 'treciadienis', 'ketvirtadienis', 'penktadienis'];
+console.log(savaite);
+
+const menesiai = ['sausis', 'vasaris', 'kovas', 'balandis', 'geguze'];
+console.log(menesiai);
+
+const sezonai = ['vasara', 'pavasaris', 'ziema', 'ruduo', 'atostogos'];
+console.log(sezonai);
