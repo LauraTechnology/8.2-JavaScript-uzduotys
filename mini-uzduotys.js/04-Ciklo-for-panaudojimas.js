ffd4b7fa7ec842bacbb42b6e81900abe8f88843c
LauraTechnology <86227135+LauraTechnology@users.noreply.github.com>
2021-09-20 10:02:48 +0300

    Pradedu spręsti ciklo FOR (for loop) užduotis

--- mini-uzduotys.js/04-Ciklo-for-panaudojimas.js
+++ mini-uzduotys.js/04-Ciklo-for-panaudojimas.js
@@ -5,8 +5,42 @@
 //d.	574 - 815
 //e.	-50 - 50
 //f.	-70 - 30
+
+for (let skaičius = 0 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //2.	panaudojant ciklą perrašyti tekstinio tipo kintamųjų reikšmes iš kito galo:
 //a.	pvz.: “abcdef” -> “fedcba”
+
 //3.	Suskaičiuoti, kiek nurodytame intervale yra skaičių, kurie dalijasi be liekanos iš 3, 5 ir 7 atskirai:
 //a.	0 - 11
 //b.	8 - 31
